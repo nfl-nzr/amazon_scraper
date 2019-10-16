7bc336c7af0aebc865e1acef6e2610489466471d
nowfal-n <nowfal@metrictreelabs.com>
2019-10-16 18:36:36 +0530

    ejs templates

--- scraper.js
+++ scraper.js
@@ -3,14 +3,13 @@ const _getPageSource = require('./helpers/api')._getPageSource;
 const _parsePriceFromSource = require('./helpers/parser')._parsePriceFromSource;
 
 async function _main() {
-    console.log('>>>')
-    // const products = _dbOps.getAllProducts();
-    // const sourcePromises = products.map(ele => _getPageSource(_dbOps.getById(ele.product_uuid).url, ele.product_uuid));
-    // Promise.all(sourcePromises)
-    //     .then(sources => sources.map(source => _parsePriceFromSource(source.html, source.id)))
-    //     .then(pricePromises => Promise.all(pricePromises))
-    //     .then(data => _dbOps.updateJob(data))
-    //     .catch(err => console.log(err));
+    const products = _dbOps.getAllProducts();
+    const sourcePromises = products.map(ele => _getPageSource(_dbOps.getById(ele.product_uuid).url, ele.product_uuid));
+    Promise.all(sourcePromises)
+        .then(sources => sources.map(source => _parsePriceFromSource(source.html, source.id)))
+        .then(pricePromises => Promise.all(pricePromises))
+        .then(data => _dbOps.updateJob(data))
+        .catch(err => console.log(err));
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -13,16 +13,16 @@ const _dbOps = require('./helpers/db');
 
 const views = join(__dirname, 'views');
 
-cron.schedule('*/1 * * * *', () => {
-    console.log(new Date())
+cron.schedule('*/5 * * * * *', () => {
+    scrape()
 });
 
 const app = polka().listen(3000);
 
 app.get('/products', (req, res) => {
     res.render = () => {
-        console.log('>>')
-        renderFile(join(views, 'pages/products.ejs'), {}, (err, html) => {
+        const products = _dbOps.getAllProducts();
+        renderFile(join(views, 'pages/products.ejs'), { products }, (err, html) => {
             // Handle Error, else return output
             if (err) return send(res, 500, err.message || err);
             res.setHeader('content-type', 'text/html');
@@ -32,5 +32,14 @@ app.get('/products', (req, res) => {
     res.render()
 });
 app.get('/products/:id/timeline', (req, res) => {
-    send(res, 200, _dbOps.getProductAndTimeline(req.params.id), JSON_HEADER)
+    res.render = () => {
+        const products = _dbOps.getAllProducts();
+        renderFile(join(views, 'pages/products.ejs'), { products }, (err, html) => {
+            // Handle Error, else return output
+            if (err) return send(res, 500, err.message || err);
+            res.setHeader('content-type', 'text/html');
+            send(res, 200, html);
+        });
+    }
+    res.render()
 });
