9cabb6f8ed09a75f14501309d6e97f7c10892af6
athulnath <athul@metrictreelabs.com>
2019-10-16 22:32:46 +0530

    added index route and changed url

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ const views = join(__dirname, 'views');
 
 const app = polka().listen(PORT, ()=>console.log('App listening on ' + PORT));
 
+app.get('/', (req, res) => {
+    res.render = () => {
+        const products = _dbOps.getAllProducts();
+        renderFile(join(views, 'pages/index.ejs'), { products }, (err, html) => {
+            // Handle Error, else return output
+            if (err) return send(res, 500, err.message || err);
+            res.setHeader('content-type', 'text/html');
+            send(res, 200, html);
+        });
+    }
+    res.render()
+});
+
 app.get('/products', (req, res) => {
     res.render = () => {
         const products = _dbOps.getAllProducts();
