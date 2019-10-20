f71394dc93574c853ebef5342146a2319a74361f
Nowfal112 <nowfal_n@yahoo.com>
2019-10-20 22:24:51 +0530

    switch from heroku to openode

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         $("#spinner").toggle();
         const productId = $(this).attr("data-attr-id");
         $.ajax({
-            url: `https://blue-snow-scraper.herokuapp.com/${productId}/timeline`,
+            url: `http://pryspy.openode.io/${productId}/timeline`,
             success: function (response) {
                 const { priceData, labels, product, pointBackgroundColor } = response;
                 $("#spinner").toggle();
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
             type: 'post',
             data: formData,
-            url: 'https://blue-snow-scraper.herokuapp.com',
+            url: 'http://pryspy.openode.io/',
             dataType: 'json',
             encode: true,
             success: function (response) {
